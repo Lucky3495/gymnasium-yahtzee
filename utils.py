cd1943fdf4ea5899a7e687af9cd9f72fb9b768fd
Saeed Bawazeer <sab3495@gmail.com>
2023-09-04 01:23:04 +0300

    Add Sheet class
    Added sheet class, its constructor, and function to return empty categories

--- utils.py
+++ utils.py
@@ -19,3 +19,26 @@ class Category(IntEnum):
     LARGE_STRAIGHT = 10
     YAHTZEE = 11
     CHOICE = 12
+
+class Sheet:
+    """
+    A class that represents a Yahtzee sheet.
+    """
+    def __init__(self) -> None:
+        """
+        Initilizes a Sheet, will start empty.
+        """
+        self._round: int = 0
+        self._sheet: list[int] = [-1]*13 # -1 means the category is empty, 0 means it is filled with 0
+    
+    
+    def get_empty_categories(self) -> list[Category]:
+        """
+        Returns a list with all empty categories.
+        """
+        empty = []
+        for value in Category:
+            if self._sheet[value] == -1:
+                empty.append(value)
+        
+        return empty
