03a02d31e91ef78ffa21d4807b4b1c7374dafc74
Saeed Bawazeer <sab3495@gmail.com>
2023-09-04 22:29:29 +0300

    Update yahtzee.py

--- yahtzee.py
+++ yahtzee.py
@@ -1,5 +1,5 @@
 from utils import *
-import numpy as np
+from numpy.random import randint as np_randint
 from random import randint
 
 class Yahtzee:
@@ -8,7 +8,7 @@ class Yahtzee:
         self._sheet = Sheet()
         self.rule = rule
         self.round = 0 # rounds will have the range [0, 12], when round == 13 the game is over
-        self.dice = list(np.random.randint(low=1, high=7, size=5))
+        self.dice = list(np_randint(low=1, high=7, size=5))
         self.rolls = 2 # number of rolls remaining, 0 means no rolls, must choose a category. (Rounds start with dice already rolled for first time)
 
     def reroll(self, mask: list[bool]) -> None:    
