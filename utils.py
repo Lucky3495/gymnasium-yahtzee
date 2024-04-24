277bb919001f8cd7fbf0ca01a31f3c6002582294
Saeed Bawazeer <sab3495@gmail.com>
2024-04-24 11:29:55 +0300

    Doc and Name Changes
    Changed the name of Rule to be JokerRule since it makes more sense.
    Also, added some comments to explain the different rules.

--- utils.py
+++ utils.py
@@ -1,9 +1,28 @@
 from enum import Enum, IntEnum
 from scoring import function_list
 
-class Rule(Enum):
+class JokerRule(Enum):
+    # The rules correspond to when a second Yahtzee is gotten, the second Yahtzee is a "Joker"
+    # Upper section is: Ones, Twos, ..., Sixes
+    # Lower section is the rest
+
+    # Forced Joker:
+    # 1. Upper section box corresponding to Yahtzee must be used if unused
+    # 2. If the corresponding upper section box was used, 
+    #    a lower section box can be used, score is calculated normally
+    # 3. If all lower sections are already used,
+    #    then an upper box section must be used, it will score 0
     FORCED_JOKER = 0
+
+    # Free Choice Joker:
+    # Player can choose any box, only if the upper corresponding box has already been used,
+    # otherwise it would score 0 if it's used in the lower section's full house, small and large straigt.
+    # (i am not sure about the three of a kind, four of a kind, and chance, wikipedia doesn't mention them)
     FREE_JOKER = 1
+
+    # Original Joker:
+    # Jokers can only be used in the lower section, this was changed later to idk what
+    # (this rule sucks the most, probably won't ever use it, maybe not implement it either)
     ORIGINAL_JOKER = 2
 
 class Category(IntEnum):
--- yahtzee.py
+++ yahtzee.py
@@ -5,9 +5,9 @@ from random import randint
 
 class Yahtzee:
 
-    def __init__(self, rule: Rule=Rule.FORCED_JOKER) -> None:
+    def __init__(self, rule: JokerRule=JokerRule.FORCED_JOKER) -> None:
         self.sheet: Sheet = Sheet()
-        self.rule: Rule = rule
+        self.rule: JokerRule = rule
         self.round: int = 0 # rounds will have the range [0, 12], when round == 13 the game is over
         self.dice: list[int] = list(np_randint(low=1, high=7, size=5))
         self.rolls: int = 2 # number of rolls remaining, 0 means no rolls, must choose a category. (Rounds start with dice already rolled for first time)
