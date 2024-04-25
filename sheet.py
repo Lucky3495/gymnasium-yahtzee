afadf973b9f8ac5231d62584d811e0c6348c8c4c
Saeed Bawazeer <sab3495@gmail.com>
2024-04-25 15:01:18 +0300

    Create sheet.py and modify yahtzee.py
    Added Sheet to its own file. Changed the import in yahtzee.py

---
+++ sheet.py
@@ -0,0 +1,30 @@
+class Sheet:
+    """
+    A class that represents a Yahtzee sheet.
+    """
+    def __init__(self) -> None:
+        """
+        Initilizes a Sheet, will start empty.
+        """
+        self._round: int = 0
+        self._sheet: list[int] = [-1]*13 # -1 means the category is empty, 0 means it is filled with 0
+    
+    
+    def get_empty_categories(self) -> list[Category]:
+        """
+        Returns a list with all empty categories.
+        """
+        empty = []
+        for value in Category:
+            if self._sheet[value] == -1:
+                empty.append(value)
+        
+        return empty
+    
+    def __getitem__(self, i):
+        assert i >= 0 and i <= 12
+        return self._sheet[i]
+    
+    def __setitem__(self, i, value: int):
+        assert i >= 0 and i <= 12
+        self._sheet[i] = value
